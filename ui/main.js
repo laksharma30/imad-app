0f6dabb6c1a5777517f6b2202b3d38aa4a71705b
laksharma30 <30688242+laksharma30@users.noreply.github.com>
2017-08-04 18:55:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ element.innerHTML = 'New InnerHTML';
 
 
 var img = document.getElementById('madi');
+marginRight=0;
+function moveLeft(){
+    marginRight= marginRight+10;
+    img.style.marginRight = marginRight + 'px';
+}
 img.onclick= function(){
-    img.style.marginLeft= '100px';
+    var interval = setInterval(moveLeft, 100);
+    
 }
