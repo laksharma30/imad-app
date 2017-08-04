c9f74e0338139c82c3e7ccba45b9c74c4b9aec01
laksharma30 <30688242+laksharma30@users.noreply.github.com>
2017-08-04 18:52:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'New InnerHTML';
+
+
+var img = document.getElementById('madi');
+img.onclick= function(){
+    img.style.marginLeft= '100px';
+}
