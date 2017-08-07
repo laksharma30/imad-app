861442670a6396f6ff7155cba4a6cea6bebbf2c2
laksharma30 <30688242+laksharma30@users.noreply.github.com>
2017-08-07 12:24:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,4 +20,18 @@ button.onclick = function(){
    request.open('GET', 'http://laksharma30.imad.hasura-app.io/counter', true);
    request.send(null);
     
+};
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit =  document.getElementById('submit_btn');
+submit.onclick=function(){
+
+    var name = ['Name1', 'Name2','Name3', 'Name5'];
+    var list ='';
+    for(var i=0; i<names.length;i++){
+        list += '<li>'+ names[i] + '</li>';
+    }
+    var ul= document.getElementById('namelist');
+    ul.innerHTML = list;
 }
