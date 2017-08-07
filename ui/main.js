6fd831fa3e6e23606b1758ef53d6d3e42c2c76b3
laksharma30 <30688242+laksharma30@users.noreply.github.com>
2017-08-07 12:26:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ button.onclick = function(){
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit =  document.getElementById('submit_btn');
-submit.onclick=function(){
+submit.onclick = function() {
 
     var name = ['Name1', 'Name2','Name3', 'Name5'];
     var list ='';
